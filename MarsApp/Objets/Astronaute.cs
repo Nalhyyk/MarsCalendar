19bfcae42f1f69689aa1ddf4127a219b7beabfda
Tubbbe <alexandre.monge@yahoo.fr>
2015-11-09 08:33:09 +0100

    Astronautes ont leur calendrier

--- MarsApp/Objets/Astronaute.cs
+++ MarsApp/Objets/Astronaute.cs
@@ -12,7 +12,9 @@ namespace MarsApp
     {
         private String nom;
         private String prenom;
-        private String age;
+        private int age;
+
+        private Dictionary<int, Journee> journeesMission;
 
         #region Constructeurs
         /// <summary>
@@ -21,12 +23,40 @@ namespace MarsApp
         /// <param name="nom">Nom de l'Astronaute</param>
         /// <param name="prenom">Prénom de l'Astronaute</param>
         /// <param name="age">Age de l'Astronaute</param>
-        public Astronaute(String nom, String prenom, String age)
+        public Astronaute(String nom, String prenom, int age)
         {
             this.nom = nom;
             this.prenom = prenom;
             this.age = age;
+            journeesMission = new Dictionary<int, Journee>();
         }
+        #endregion
+
+        /// <summary>
+        /// Permet de créer un emploi du temps pour l'astronaute
+        /// </summary>
+        public void creerEdT(int numJour)
+        {
+            for (int i = 1; i < 501; ++i)
+                journeesMission[i] = new Journee(i);
+
+            // Mise à jour des jours : passés | en cours | à venir
+            for (int i = 1; i < numJour; ++i)
+                journeesMission[i].journeePassee();
+            journeesMission[numJour].journeeEnCours();
+        }
+
+        public override string ToString()
+        {
+            return nom + " " + prenom;
+        }
+
+        #region Accesseurs
+        /// <summary>
+        /// Permet d'obtenir l'emploi du temps de l'astronaute
+        /// </summary>
+        /// <returns>L'emploi du temps de l'astronaute</returns>
+        public Dictionary<int, Journee> getJourneesMission() { return journeesMission; }
         #endregion
     }
 }
